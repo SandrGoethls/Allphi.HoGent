cb0f1079e59d3b7274823b878d56971039531b69
SandrGoethls <sander.goetahls2@student.hogent.be>
2024-04-10 11:27:59 +0200

    Add GetAllDiversAsync task in DriverServices

--- AllPhi.HoGent.Blazor/Services/DriverServices.cs
+++ AllPhi.HoGent.Blazor/Services/DriverServices.cs
@@ -1,4 +1,11 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using Newtonsoft.Json;
+using System.Globalization;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public class DriverServices : IDriverServices
     {
@@ -7,6 +14,28 @@
         public DriverServices(HttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
+
+        public async Task<DriverListDto> GetAllDriversAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            if (!string.IsNullOrEmpty(sortBy)) queryString["sortBy"] = sortBy;
+            queryString["isAscending"] = isAscending.ToString();
+            queryString["pageNumber"] = pageNumber.ToString();
+            queryString["pageSize"] = pageSize.ToString();
+
+            string url = $"api/drivers/getalldrivers?{queryString}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching drivers: {response.ReasonPhrase}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var driversDto = JsonConvert.DeserializeObject<DriverListDto>(responseContent);
+            return driversDto ?? new DriverListDto();
         }
     }
 }
--- AllPhi.HoGent.Blazor/Services/IDriverServices.cs
+++ AllPhi.HoGent.Blazor/Services/IDriverServices.cs
@@ -1,6 +1,10 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using System.Runtime.InteropServices;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public interface IDriverServices
     {
+        Task<DriverListDto> GetAllDriversAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize);
     }
 }
