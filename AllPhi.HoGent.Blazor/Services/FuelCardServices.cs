63181c34158571ea70543b6df4e7724d10a16dc2
SandrGoethls <sander.goetahls2@student.hogent.be>
2024-04-20 13:03:45 +0200

    Enable fuel card and vehicle loading in driver page

--- AllPhi.HoGent.Blazor/Services/FuelCardServices.cs
+++ AllPhi.HoGent.Blazor/Services/FuelCardServices.cs
@@ -1,4 +1,9 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using Newtonsoft.Json;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public class FuelCardServices : IFuelCardServices
     {
@@ -8,5 +13,28 @@
         {
             _httpClient = httpClient;
         }
+
+        public async Task<FuelCardListDto> GetAllFuelCardsAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize)
+        {
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            if (!string.IsNullOrEmpty(sortBy)) queryString["sortBy"] = sortBy;
+            queryString["isAscending"] = isAscending.ToString();
+            queryString["pageNumber"] = pageNumber.ToString();
+            queryString["pageSize"] = pageSize.ToString();
+
+            string url = $"api/fuelcards/getallfuelcards?{queryString}";
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new HttpRequestException($"Error fetching fuel cards: {response.ReasonPhrase}");
+            }
+
+            var responseContent = await response.Content.ReadAsStringAsync();
+            var fuelCardsDto = JsonConvert.DeserializeObject<FuelCardListDto>(responseContent);
+            return fuelCardsDto ?? new FuelCardListDto();
+        }
+
     }
 }
--- AllPhi.HoGent.Blazor/Services/IFuelCardServices.cs
+++ AllPhi.HoGent.Blazor/Services/IFuelCardServices.cs
@@ -1,6 +1,10 @@
-﻿namespace AllPhi.HoGent.Blazor.Services
+﻿using AllPhi.HoGent.Blazor.Dto;
+using System.Runtime.InteropServices;
+
+namespace AllPhi.HoGent.Blazor.Services
 {
     public interface IFuelCardServices
     {
+        Task<FuelCardListDto> GetAllFuelCardsAsync([Optional] string? sortBy, [Optional] bool isAscending, [Optional] int pageNumber, [Optional] int pageSize);
     }
 }
