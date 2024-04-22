c3948cc89c98f09ce6d871d696edffd90d658a70
Yalcin1991 <yalcin.narin@cloudware.be>
2024-04-22 02:18:30 +0200

    wip

--- AllPhi.HoGent.Blazor/Extansions/NumberOfDoorsExtensions.cs
+++ AllPhi.HoGent.Blazor/Extansions/NumberOfDoorsExtensions.cs
@@ -5,15 +5,24 @@ namespace AllPhi.HoGent.Blazor.Extansions
     public static class NumberOfDoorsExtensions
     {
         private static readonly Dictionary<NumberOfDoors, string> _doorNames = new Dictionary<NumberOfDoors, string>
-    {
-        { NumberOfDoors.OneDoor, "1 deur" },
-        { NumberOfDoors.TwooDoors, "2 deuren" },
-        { NumberOfDoors.ThreeDoors, "3 deuren" },
-        { NumberOfDoors.FourDoors, "4 deuren" },
-        { NumberOfDoors.FiveDoors, "5 deuren" }
-    };
+        {
+            { NumberOfDoors.OneDoor, "1 deur" },
+            { NumberOfDoors.TwooDoors, "2 deuren" },
+            { NumberOfDoors.ThreeDoors, "3 deuren" },
+            { NumberOfDoors.FourDoors, "4 deuren" },
+            { NumberOfDoors.FiveDoors, "5 deuren" }
+        };
 
-        // Methode om de volledige dictionary terug te geven
+        public static string GetDescription(this NumberOfDoors numberOfDoors)
+        {
+            if (_doorNames.TryGetValue(numberOfDoors, out var description))
+            {
+                return description;
+            }
+
+            return "Onbekend aantal deuren";
+        }
+
         public static Dictionary<NumberOfDoors, string> GetAllDoorDescriptions()
         {
             return _doorNames;
