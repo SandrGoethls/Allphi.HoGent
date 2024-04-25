5bc318e71cd39da2425a7d62478d2e2bdfab3f46
KenGod88 <ken.godfroid@student.hogent.be>
2024-04-25 21:44:27 +0200

    Added DriverVehicleListDto to Blazor project for Chart functionality

---
+++ AllPhi.HoGent.Blazor/Dto/DriverVehicleListDto.cs
@@ -0,0 +1,9 @@
+﻿namespace AllPhi.HoGent.Blazor.Dto
+{
+    public class DriverVehicleListDto
+    {
+        public List<DriverVehicleDto> DriverVehicleDtos { get; set; }
+        public int TotalItems { get; set; }
+    }
+}
+
