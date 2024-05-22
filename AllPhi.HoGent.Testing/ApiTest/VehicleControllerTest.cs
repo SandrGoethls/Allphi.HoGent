a66d2aecf2ff142f83d905ebf346c7aead4e330f
SandrGoethls <sander.goetahls2@student.hogent.be>
2024-05-22 19:15:33 +0200

    Enable VehicleControllerTests

--- AllPhi.HoGent.Testing/ApiTest/VehicleControllerTest.cs
+++ AllPhi.HoGent.Testing/ApiTest/VehicleControllerTest.cs
@@ -17,25 +17,6 @@ namespace AllPhi.HoGent.Testing.ApiTest
     public class VehicleControllerTest
     {
         [Fact]
-        public async Task GetVehicleById_ReturnsOk()
-        {
-            #region Arrange
-            var vehicleStore = VehicleStoreMock.GetVehicleStoreMock();
-            var driverVehicleStore = DriverVehicleStoreMock.GetDriverVehicleStoreMock();
-            var controller = new VehiclesController(vehicleStore.Object, driverVehicleStore.Object);
-            var vehicleId = new Guid("a7245037-c683-4f82-b261-5c053502ed93");
-            #endregion
-
-            #region Act
-            var result = await controller.GetVehicleById(vehicleId);
-            #endregion
-
-            #region Assert
-            Assert.IsType<OkObjectResult>(result.Result);
-            #endregion
-        }
-
-        [Fact]
         public async Task GetVehicleById_ReturnsNotFound()
         {
             #region Arrange
